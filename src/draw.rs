2d1b8c05b850dd2184d48ab6aa575f45989f21de
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2022-02-08 15:37:31 +0900

    Implement aux lines

--- src/draw.rs
+++ src/draw.rs
@@ -11,6 +11,25 @@ use env_logger::{Builder, Env};
 
 type Point = (f32, f32);
 
+trait FromPoint<S> {
+    fn from_points(p1: lyon_geom::Point<S>, p2: lyon_geom::Point<S>) -> Self
+    where
+        S: std::ops::Sub<Output = S> + Clone;
+}
+
+impl<S> FromPoint<S> for lyon_geom::Line<S> {
+    fn from_points(p1: lyon_geom::Point<S>, p2: lyon_geom::Point<S>) -> Self
+    where
+        S: std::ops::Sub<Output = S> + Clone,
+    {
+        let v = p2 - p1.clone();
+        lyon_geom::Line {
+            point: p1,
+            vector: v,
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 struct Flags {}
 
@@ -24,6 +43,7 @@ struct Shape {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[allow(non_snake_case)]
 struct PointData {
     version: String,
     flags: Flags,
@@ -38,11 +58,9 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-fn img2base64(img: &image::GrayImage) -> String {
+fn img2base64(img: &image::DynamicImage) -> String {
     let mut buf = Vec::new();
-    let base_img = image::open("dicom.png").unwrap();
-    base_img
-        .write_to(&mut buf, image::ImageOutputFormat::Png)
+    img.write_to(&mut buf, image::ImageOutputFormat::Png)
         .unwrap();
     base64::encode(&buf)
 }
@@ -75,7 +93,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // set background
     let base_img = image::open("dicom.png").unwrap();
-    let res_base64 = img2base64(&base_img.to_luma8());
+    let res_base64 = img2base64(&base_img);
     let b64 = "data:image/png;base64,".to_owned() + &res_base64;
     let bg = element::Image::new()
         .set("x", 0)
@@ -89,42 +107,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // draw lines
     let line_width = 10;
     let line_color = "yellow";
-    let image_box = lyon_geom::Box2D::new(
-        lyon_geom::Point::new(0f32, 0f32),
-        lyon_geom::Point::new(image_width as f32, image_height as f32),
-    );
     let tl = lyon_geom::Point::new(0f32, 0f32);
     let tr = lyon_geom::Point::new(image_width as f32, 0f32);
-    let top_line = lyon_geom::Line {
-        point: tl,
-        vector: tr - tl,
-    };
-
     let bl = lyon_geom::Point::new(0f32, image_height as f32);
     let br = lyon_geom::Point::new(image_width as f32, image_height as f32);
-    let bottom_line = lyon_geom::Line {
-        point: bl,
-        vector: br - bl,
-    };
-    let left_line = lyon_geom::Line {
-        point: tl,
-        vector: bl - tl,
-    };
-    let right_line = lyon_geom::Line {
-        point: tr,
-        vector: br - tr,
-    };
+    let top_line = lyon_geom::Line::from_points(tl, tr);
+    let bottom_line = lyon_geom::Line::from_points(bl, br);
+    let left_line = lyon_geom::Line::from_points(tl, bl);
+    let right_line = lyon_geom::Line::from_points(tr, br);
     let points: Vec<Point> = labels.iter().map(|l| shape_map[*l]).collect();
     let mut group = element::Group::new()
         .set("stroke", line_color)
         .set("stroke-width", line_width);
-    for (p1, p2) in vec![(points[0], points[1]), (points[2], points[3])] {
-        let p = lyon_geom::Point::new(p1.0, p1.1);
-        let v = p - lyon_geom::Point::new(p2.0, p2.1);
-        let line = lyon_geom::Line {
-            point: p,
-            vector: v,
-        };
+
+    let (p1, p2) = (points[0], points[1]);
+    let c2_line = lyon_geom::Line::from_points(
+        lyon_geom::Point::new(p1.0, p1.1),
+        lyon_geom::Point::new(p2.0, p2.1),
+    );
+    let (p1, p2) = (points[2], points[3]);
+    let c7_line = lyon_geom::Line::from_points(
+        lyon_geom::Point::new(p1.0, p1.1),
+        lyon_geom::Point::new(p2.0, p2.1),
+    );
+    for line in vec![c2_line, c7_line] {
+        let v = &line.vector;
         let (int1, int2) = if (v.y / v.x) > 0.5 {
             (
                 line.intersection(&top_line).unwrap(),
@@ -143,13 +150,71 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .set("y2", int2.y);
         group = group.add(line);
     }
+    let angle_degree = c2_line.vector.angle_to(c7_line.vector).to_degrees();
+    let intersect = c2_line.intersection(&c7_line).unwrap();
+    let sign = if intersect.x > points[0].0 { 1.0 } else { -1.0 };
+    let angle_degree = sign * angle_degree;
+    let angle = format!("{:.1}°", angle_degree);
+    debug!("angle {}", angle);
+
+    let font_size = "64";
+    let font_family = "serif";
+
+    if lyon_geom::Box2D::new(tl, br).contains(intersect) {
+        debug!("Inside");
+        let text_node = node::Text::new(angle);
+        let text = element::Text::new()
+            .set("x", intersect.x)
+            .set("y", intersect.y)
+            .set("font-family", font_family)
+            .set("font-size", font_size)
+            .set("fill", "black")
+            .add(text_node);
+        document = document.add(text);
+    } else {
+        debug!("Draw aux lines");
+        let a = lyon_geom::Point::new(points[0].0, points[0].1);
+        let p = lyon_geom::Point::new(points[1].0, points[1].1);
+        let aux_c2 = p + (p - a) / 2.0;
+        let t_origin = lyon_geom::Transform::translation(-intersect.x, -intersect.y);
+        let mut rog_angle = c2_line.vector.angle_to(c7_line.vector);
+        rog_angle.radians /= 2.0;
+        let t = t_origin
+            .then_rotate(rog_angle)
+            .then_translate(lyon_geom::Vector::new(intersect.x, intersect.y));
+        let aux_int = t.transform_point(aux_c2);
+        debug!("Aux intersection {:?}", aux_int);
+        for (line_self, line_other) in vec![(c2_line, c7_line), (c7_line, c2_line)] {
+            let aux_on_line = line_self.equation().project_point(&aux_int);
+            let aux_p1 = aux_on_line.lerp(
+                lyon_geom::Line::from_points(aux_on_line, aux_int)
+                    .intersection(&line_other)
+                    .unwrap(),
+                0.75,
+            );
+            let line = element::Line::new()
+                .set("x1", aux_p1.x)
+                .set("y1", aux_p1.y)
+                .set("x2", aux_on_line.x)
+                .set("y2", aux_on_line.y);
+            group = group.add(line);
+        }
+        let text_node = node::Text::new(angle);
+        let text = element::Text::new()
+            .set("x", aux_int.x)
+            .set("y", aux_int.y)
+            .set("font-family", font_family)
+            .set("font-size", font_size)
+            .set("fill", "black")
+            .add(text_node);
+        document = document.add(text);
+    }
+
     document = document.add(group);
 
     // draw points
     let colors = vec!["red", "lime", "blue", "magenta"];
     let point_radius = "10";
-    let font_size = "64";
-    let font_family = "serif";
     for (label, color) in labels.into_iter().zip(colors) {
         let point_xy = shape_map[label];
         let circle = Circle::new()
