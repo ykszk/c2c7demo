e1074c603a662c03c2c257e559f2168d006df77e
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2022-02-17 18:03:19 +0900

    Add error display in react

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -53,8 +53,8 @@ function App() {
     })
   }, [])
   const [inputImage, setInputImage] = useState(ImageFile.default());
+  const [errMessage, setErrMessage] = useState("");
   const [measureDisabled, setMeasureDisabled] = useState(false);
-  const [resultImage, setResultImage] = useState("");
   const [fgSrc, setFgSrc] = useState("");
   const processImage = async (file: File) => {
     const filename = file.name;
@@ -69,96 +69,98 @@ function App() {
         setInputImage(new ImageFile(filename, imgb64.b64, imgb64.width, imgb64.height, arr));
       } catch (error) {
         console.error(error);
+        setErrMessage(error as string);
         setInputImage(ImageFile.default());
       }
     }
   }
   const blob = new Blob([fgSrc], { type: 'image/svg+xml' });
   const fgURL = URL.createObjectURL(blob);
+  const reset_button = (
+    <button title="Restart from the start" onClick={() => { setInputImage(ImageFile.default()); setFgSrc(""); setErrMessage(""); setMeasureDisabled(false) }}>Reset</button>
+  )
+  if (errMessage.length !== 0) {
+    return (
+      <div className="App">
+        <p className="error">{errMessage}</p>
+        {reset_button}
+      </div>
+    )
+  } else if (inputImage.is_default()) {
+    return (
+      <div className="App">
+        <Dropzone onDrop={async acceptedFiles => processImage(acceptedFiles[0])}>
+          {({ getRootProps, getInputProps }) => (
+            <section>
+              <div className="dropzone" {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>Drag input image, or click to choose a file. PNG, JPEG or DICOM (experimental).</p>
+              </div>
+            </section>
+          )}
+        </Dropzone>
+      </div>
+    )
+  }
   return (
     <div className="App">
-      {
-        !inputImage.is_default() ?
-          <div className="main">
+      <div className="main">
+        <div>
+          <h3>Input Image</h3>
+          <img alt="input" src={inputImage.b64} />
+        </div>
+        {fgSrc.length ?
+          <>
             <div>
-              <h3>Input Image</h3>
-              <img alt="input" src={inputImage.b64} />
-            </div>
-            {resultImage.length ?
-              <>
-                <div>
-                  <h3>Result</h3>
-                  <div className="overlay">
-                    <img alt="background" className="bg" src={inputImage.b64} />
-                    <img alt="foreground" className="fg" src={fgURL} />
-                  </div>
-                </div>
-                <div className="buttons">
-
-                  <label>
-                    <input type="checkbox" />
-                    Lines
-                  </label>
-                  <label>
-                    <input type="checkbox" />
-                    Points
-                  </label>
-                  <label>
-                    <input type="checkbox" />
-                    Heatmap
-                  </label>
-
-                  <button title="Save output">Save</button>
-                  <button title="Restart from the start" onClick={() => { setInputImage(ImageFile.default()); setResultImage("") }}>Reset</button>
-                </div>
-              </>
-              :
-              <div className="buttons">
-                <button title="Let the AI do some job." disabled={measureDisabled && inputImage.is_default()}
-                  onClick={(event) => {
-                    setMeasureDisabled(true);
-                    console.log("Create onnx session")
-                    ort.InferenceSession.create(
-                      "c2c7.onnx",
-                      {
-                        executionProviders: ["webgl"], graphOptimizationLevel: 'all'
-                      }
-                    ).then(session => {
-                      const tensor_raw = create_input_tensor(inputImage.arr as Uint8Array);
-                      const tensor_width = calc_tensor_width(inputImage.width, inputImage.height);
-                      const dims = [2, 1, 768, tensor_width];
-                      console.log(inputImage.width, dims);
-                      const input_tensor = new ort.Tensor("float32", tensor_raw, dims)
-                      console.log('Run inference');
-                      runInference(session, input_tensor).then(v => {
-                        const [results, inferenceTime] = v;
-                        console.log(results.length, inferenceTime);
-                        const svg_str = process_output(results, tensor_width, inputImage.width, inputImage.height);
-                        setFgSrc(svg_str)
-                        setResultImage("yes")
-                      });
-                    }
-                    )
-
-                  }}>Measure
-                </button>
-                <p className="smallNote">Takes about 5 - 30 seconds.</p>
+              <h3>Result</h3>
+              <div className="overlay">
+                <img alt="background" className="bg" src={inputImage.b64} />
+                <img alt="foreground" className="fg" src={fgURL} />
               </div>
-            }
-
-          </div>
+            </div>
+            <div className="buttons">
+              <button title="Save output">Save</button>
+              {reset_button}
+            </div>
+          </>
           :
-          <Dropzone onDrop={acceptedFiles => { processImage(acceptedFiles[0]) }}>
-            {({ getRootProps, getInputProps }) => (
-              <section>
-                <div className="dropzone" {...getRootProps()}>
-                  <input {...getInputProps()} />
-                  <p>Drag input image, or click to choose a file. PNG, JPEG or DICOM (experimental).</p>
-                </div>
-              </section>
-            )}
-          </Dropzone>
-      }
+          <div className="buttons">
+            <button title="Let the AI do some job." disabled={measureDisabled}
+              onClick={(event) => {
+                try {
+                  setMeasureDisabled(true);
+                  console.log("Create onnx session")
+                  ort.InferenceSession.create(
+                    "c2c7.onnx",
+                    {
+                      executionProviders: ["webgl"], graphOptimizationLevel: 'all'
+                    }
+                  ).then(session => {
+                    const tensor_raw = create_input_tensor(inputImage.arr as Uint8Array);
+                    const tensor_width = calc_tensor_width(inputImage.width, inputImage.height);
+                    const dims = [2, 1, 768, tensor_width];
+                    console.log(inputImage.width, dims);
+                    const input_tensor = new ort.Tensor("float32", tensor_raw, dims)
+                    console.log('Run inference');
+                    runInference(session, input_tensor).then(v => {
+                      const [results, inferenceTime] = v;
+                      console.log(results.length, inferenceTime);
+                      const svg_str = process_output(results, tensor_width, inputImage.width, inputImage.height);
+                      setFgSrc(svg_str)
+                    }).catch(reason => { console.error(reason); setErrMessage("Failed to run the inference.") });
+                  }
+                  ).catch(reason => { console.error(reason); setErrMessage("Failed to create a onnx session.") })
+                } catch (error) {
+                  console.error(error);
+                  setErrMessage(error as string);
+                }
+
+              }}> {measureDisabled ? "Please wait. Measuring takes about 5 - 30 seconds." : "Measure"}
+            </button>
+            {measureDisabled ? "" : reset_button}
+          </div>
+        }
+      </div>
     </div >
   );
 }
